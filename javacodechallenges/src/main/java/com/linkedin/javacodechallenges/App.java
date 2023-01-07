4ac8a1f6db458aee99f8baa9592b014dc1f18f4d
Anuda Upek <31981160+AnudaUpek@users.noreply.github.com>
2023-01-07 16:00:33 +0000

    sloved

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -6,18 +6,16 @@ public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
         // TODO: Implement method
-        int cc = 0;
+        
         if(gallonsUsage < 1497){
             return 18.84;
         } else {
-            int ccfs = (int) (gallonsUsage -1496)/748;
+            double ccfs = Math.ceil((gallonsUsage - 1496) / 748);
             // System.out.println(ccfs);
-            cc = Math.round(ccfs);
-            if((gallonsUsage-1496)%748 != 0){
-                cc += 1;
-            }
+            
+            
             //System.out.println(cc);
-            double bill = 18.84 + (cc * 3.90);
+            double bill = 18.84 + (ccfs * 3.90);
             //System.out.println(cc);
             return bill;
         }
