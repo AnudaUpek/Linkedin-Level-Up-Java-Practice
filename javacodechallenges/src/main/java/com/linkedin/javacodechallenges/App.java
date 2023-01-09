a80f128d7b26b40a062fab20fb400e00498e35be
Anuda Upek <31981160+AnudaUpek@users.noreply.github.com>
2023-01-09 05:54:23 +0000

    Sloved

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person p1 = new Person("Anuda", "Upek",22);
+        Person p2 = new Person("Amal", "Pera", 42);
+        Person p3 = new Person("Amila", "Upek", 30);
+
+        p1.intro();
+        p2.intro();
+        p3.intro();
+       
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,41 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
+  private String fname;
+  private String lname;
+  private int age;
+  
+    
+  public Person(String fname, String lname, int age) {
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+  }
+
+ 
+
+  public void intro(){
+    System.out.println("Details : \n First Name : " + getFname() + "\n Last Name : " + getLname() + "\n Age : " + getAge());
+  }
+
+  public String getFname() {
+    return fname;
+  }
+  public void setFname(String fname) {
+    this.fname = fname;
+  }
+  public String getLname() {
+    return lname;
+  }
+  public void setLname(String lname) {
+    this.lname = lname;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+
   
 }
